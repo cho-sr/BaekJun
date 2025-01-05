6e2f015fbaf0a7ed88301eb83439dad6fd48cdfa
조석래 <cho020218@gmail.com>
2025-01-05 23:33:43 +0900

    Add files via upload

---
+++ Py/30802.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+size = list(map(int,input().split()))
+T, P = map(int,input().split())
+
+cnt = 0
+for s in size:
+    if s ==0:
+        continue
+    elif s % T ==0:
+        cnt += s // T
+    else:
+        cnt += s // T+1
+print(cnt)
+print((N//P),N%P)
